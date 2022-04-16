0791f7070af247c13aedd600f3d4fdc559c20aef
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 11:49:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('Lista de Reproduccion');
+
+alert('Hola, Bienvenide, cree su lista que reproduccion aqui');
+
+const nombre = prompt('Ingrese el nombre de su lista');
+const cancion01 = prompt('Ingrese el nombre de la cancion');
+const cancion02 = prompt('Ingrese el nombre de la cancion');
+const cancion03 = prompt('Ingrese el nombre de la cancion');
+
+alert (`Se ha creado la playlist ${nombre}, con las canciones '${cancion01}', '${cancion02}' y '${cancion03}'`);
